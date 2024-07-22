106f2ce94d00540c9ca5170866bdb2462ffdc11a
dappon4 <dappon.04@gmail.com>
2024-07-22 22:07:55 +0900

    added base module

---
+++ nn/Module.py
@@ -0,0 +1,20 @@
+import jax.numpy as jnp
+from jax import grad, jit, vmap
+
+class Module():
+    def __init__(self) -> None:
+        self.training = True
+    
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}()"
+    
+    def forward(self, x):
+        return x
+    
+    def train(self) -> None:
+        self.training = True
+    
+    def eval(self) -> None:
+        self.training = False
+    
+    
